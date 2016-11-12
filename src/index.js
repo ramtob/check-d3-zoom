df0399c976fc38544a167ee9e9306e6161c51a21
ramtob <ramtob@gmail.com>
2016-11-12 22:33:09 +0200

    adding d3 modules (the app almost works)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import styles from './app.css';
 import app from './app.js';
-
+const d3 = Object.assign({},
+    require("d3-force"),
+    require("d3-selection"),
+    require("d3-scale"),
+    require("d3-drag"),
+    require("d3-array"),
+    require("d3-zoom")
+);
 app(d3);
