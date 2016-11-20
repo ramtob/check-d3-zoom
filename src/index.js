5efa28f2e6b9e63df73dbbd3eb2b927b846e1794
ramtob <ramtob@gmail.com>
2016-11-20 23:42:09 +0200

    A bit of pruning (further pruning didn't work)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import styles from './app.css';
 import app from './app.js';
-import * as sel from 'd3-selection';
+//import * as sel from 'd3-selection';
 let d3 = Object.assign({},
-    sel,
+    //sel,
     require("d3-force"),
-//    require("d3-selection"),
+    require("d3-selection"),
     require("d3-scale"),
     require("d3-drag"),
     require("d3-array"),
